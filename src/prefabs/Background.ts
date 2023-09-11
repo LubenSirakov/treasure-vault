434de711c9553d4cdb1239690f5f072c8d439920
Luben Sirakov <lubo95sirakov@gmail.com>
2023-09-11 09:58:41 +0300

    Remove unused import

--- src/prefabs/Background.ts
+++ src/prefabs/Background.ts
@@ -1,4 +1,4 @@
-import { Container, Texture, Ticker, TilingSprite } from "pixi.js";
+import { Container, Texture, TilingSprite } from "pixi.js";
 import { centerObjects } from "../utils/misc";
 
 export type BgConfig = {
